99f25397316dd8bb3364817e6d7677df217b54b5
Cruise <wangwengong@163.com>
2017-11-02 11:21:54 +0800

    hello

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=0; i<10;i++) {
+			System.out.println("This is the "+i+" line!!!");
+		}
+
+	}
+
+}
